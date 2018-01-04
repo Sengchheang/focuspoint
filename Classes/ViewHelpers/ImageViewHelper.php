f388c6a94be487c7e51c31d5f00addb9465f348d
Sengchheang Chhun <sengchheang@live.com>
2018-01-04 17:13:17 +0700

    [WORKAROUND] Argument "src" has already been defined

--- Classes/ViewHelpers/ImageViewHelper.php
+++ Classes/ViewHelpers/ImageViewHelper.php
@@ -1,116 +1,97 @@
 <?php
-/**
- * Override the ViewHelper with ratio.
- */
-
 namespace HDNET\Focuspoint\ViewHelpers;
 
-use HDNET\Focuspoint\Service\FocusCropService;
-use TYPO3\CMS\Core\Resource\FileInterface;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Domain\Model\AbstractFileFolder;
+/*                                                                        *
+ * This script is part of the TYPO3 project - inspiring people to share!  *
+ *                                                                        *
+ * TYPO3 is free software; you can redistribute it and/or modify it under *
+ * the terms of the GNU General Public License version 2 as published by  *
+ * the Free Software Foundation.                                          *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
+ * Public License for more details.                                       *
+ *                                                                        */
 
-/**
- * Override the ViewHelper with ratio.
- */
+
+use HDNET\Autoloader\Exception;
+use HDNET\Focuspoint\Service\FocusCropService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 class ImageViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\ImageViewHelper
 {
     /**
-     * Initialize ViewHelper arguments.
+     * Initialize arguments.
      */
     public function initializeArguments()
     {
-        parent::initializeArguments();
-        $this->registerArgument('ratio', 'string', 'Ratio of the image', false, '1:1');
-        $this->registerArgument('realCrop', 'boolean', 'Crop the image in real', false, true);
-        $this->registerArgument('additionalClassDiv', 'string', 'Additional class for focus point div', false, '');
+	    $this->registerArgument('ratio', 'string', 'Ratio of the image', false, '1:1');
+	    $this->registerArgument('realCrop', 'boolean', 'Crop the image in real', false, true);
+	    $this->registerArgument('additionalClassDiv', 'string', 'Additional class for focus point div', false, '');
     }
 
     /**
-     * Resize a given image (if required) and renders the respective img tag.
+     * Resizes a given image (if required) and renders the respective img tag
      *
-     * @see http://typo3.org/documentation/document-library/references/doc_core_tsref/4.2.0/view/1/5/#id4164427
-     *
-     * @param string                           $src                a path to a file, a combined FAL identifier or an uid (integer). If $treatIdAsReference is set, the integer is considered the uid of the sys_file_reference record. If you already got a FAL object, consider using the $image parameter instead
-     * @param string                           $width              width of the image. This can be a numeric value representing the fixed width of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.
-     * @param string                           $height             height of the image. This can be a numeric value representing the fixed height of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.
-     * @param int                              $minWidth           minimum width of the image
-     * @param int                              $minHeight          minimum height of the image
-     * @param int                              $maxWidth           maximum width of the image
-     * @param int                              $maxHeight          maximum height of the image
-     * @param bool                             $treatIdAsReference given src argument is a sys_file_reference record
-     * @param FileInterface|AbstractFileFolder $image              a FAL object
-     * @param string|bool                      $crop               overrule cropping of image (setting to FALSE disables the cropping set in FileReference)
-     * @param bool                             $absolute           Force absolute URL
+     * @see https://docs.typo3.org/typo3cms/TyposcriptReference/ContentObjects/Image/
      *
      * @throws \TYPO3\CMS\Fluid\Core\ViewHelper\Exception
-     *
      * @return string Rendered tag
      */
-    public function render(
-        $src = null,
-        $width = null,
-        $height = null,
-        $minWidth = null,
-        $minHeight = null,
-        $maxWidth = null,
-        $maxHeight = null,
-        $treatIdAsReference = false,
-        $image = null,
-        $crop = null,
-        $absolute = false
-    ) {
-        /** @var FocusCropService $service */
-        $service = GeneralUtility::makeInstance(FocusCropService::class);
-        $internalImage = null;
-        try {
-            $internalImage = $service->getViewHelperImage($src, $image, $treatIdAsReference);
-            if ($this->arguments['realCrop'] && $internalImage instanceof FileInterface) {
-                $src = $service->getCroppedImageSrcByFile($internalImage, $this->arguments['ratio']);
-                $treatIdAsReference = false;
-                $image = null;
-            }
-        } catch (\Exception $ex) {
-            $this->arguments['realCrop'] = true;
+    public function render()
+    {
+        if ((is_null($this->arguments['src']) && is_null($this->arguments['image'])) || (!is_null($this->arguments['src']) && !is_null($this->arguments['image']))) {
+            throw new \TYPO3\CMS\Fluid\Core\ViewHelper\Exception('You must either specify a string src or a File object.', 1382284106);
         }
+        $src = $this->arguments['src'];
+        $image = $this->arguments['image'];
+        $treatIdAsReference = $this->arguments['treatIdAsReference'];
 
-        try {
-            parent::render(
-                $src,
-                $width,
-                $height,
-                $minWidth,
-                $minHeight,
-                $maxWidth,
-                $maxHeight,
-                $treatIdAsReference,
-                $image,
-                $crop,
-                $absolute
-            );
-        } catch (\Exception $ex) {
-            return 'Missing image!';
-        }
+	    /** @var FocusCropService $service */
+	    $service = GeneralUtility::makeInstance(FocusCropService::class);
+	    $internalImage = null;
+	    try {
+		    $internalImage = $service->getViewHelperImage($this->arguments['src'], $this->arguments['image'], $this->arguments['treatIdAsReference']);
+		    if ($this->arguments['realCrop'] && $internalImage) {
+			    $src = $service->getCroppedImageSrcByFile($internalImage, $this->arguments['ratio']);
+			    $treatIdAsReference = false;
+			    $image = null;
+		    }
+	    } catch (\Exception $ex) {
+		    $this->arguments['realCrop'] = true;
+	    }
 
-        if ($this->arguments['realCrop']) {
-            return $this->tag->render();
-        }
+	    try {
+		    parent::setArguments(array(
+			    'src' => $src,
+			    'treatIdAsReference' => $treatIdAsReference,
+			    'image' => $image
+		    ));
+		    parent::render();
+	    } catch (Exception $ex) {
+		    return 'Missing image!';
+	    }
 
-        // Ratio calculation
-        if (null !== $internalImage) {
-            $focusPointY = $internalImage->getProperty('focus_point_y');
-            $focusPointX = $internalImage->getProperty('focus_point_x');
+	    if ($this->arguments['realCrop']) {
+		    return $this->tag->render();
+	    }
 
-            $additionalClassDiv = 'focuspoint';
-            if (!empty($this->arguments['additionalClassDiv'])) {
-                $additionalClassDiv .= ' ' . $this->arguments['additionalClassDiv'];
-            }
+	    // Ratio calculation
+	    if (null !== $internalImage) {
+		    $focusPointY = $internalImage->getProperty('focus_point_y');
+		    $focusPointX = $internalImage->getProperty('focus_point_x');
 
-            $focusTag = '<div class="' . $additionalClassDiv . '" data-image-imageSrc="' . $this->tag->getAttribute('src') . '" data-focus-x="' . ($focusPointX / 100) . '" data-focus-y="' . ($focusPointY / 100) . '" data-image-w="' . $this->tag->getAttribute('width') . '" data-image-h="' . $this->tag->getAttribute('height') . '">';
+		    $additionalClassDiv = 'focuspoint';
+		    if (!empty($this->arguments['additionalClassDiv'])) {
+			    $additionalClassDiv .= ' ' . $this->arguments['additionalClassDiv'];
+		    }
 
-            return $focusTag . $this->tag->render() . '</div>';
-        } else {
-            return 'Missing internal image!';
-        }
+		    $focusTag = '<div class="' . $additionalClassDiv . '" data-image-imageSrc="' . $this->tag->getAttribute('src') . '" data-focus-x="' . ($focusPointX / 100) . '" data-focus-y="' . ($focusPointY / 100) . '" data-image-w="' . $this->tag->getAttribute('width') . '" data-image-h="' . $this->tag->getAttribute('height') . '">';
+
+		    return $focusTag . $this->tag->render() . '</div>';
+	    } else {
+		    return 'Missing internal image!';
+	    }
     }
 }
